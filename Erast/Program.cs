a619920de64aa8c2e06d266dc5b982a0c11e7fb4
Kedarini <kedardamps@gmail.com>
2024-04-11 14:03:03 +0200

    Tablica

---
+++ Erast/Program.cs
@@ -0,0 +1,16 @@
+﻿class Zadanie
+{
+    static void Main(string[] args)
+    {
+        bool[] liczby = new bool[100];
+
+        WypelnienieTablicy(liczby);
+    }
+    static void WypelnienieTablicy(bool[] tab)
+    {
+        for(int i = 0; i < tab.Length; i++)
+        {
+            tab[i] = true;
+        }
+    }
+}
